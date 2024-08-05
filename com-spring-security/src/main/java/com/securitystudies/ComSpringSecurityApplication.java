1545a1a7fe9755ecf7694846343dfa1e0dac984d
luciano-mota <amoosenhorjesus3@gmail.com>
2024-08-05 07:48:26 -0300

    Commit Inicial

---
+++ com-spring-security/src/main/java/com/securitystudies/ComSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.securitystudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComSpringSecurityApplication.class, args);
+	}
+
+}
